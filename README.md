a22ac15aa25c01477db7361a1c124586de28a7a8
João Francisco Cocca Fukuda <joao.fukuda@usp.br>
2020-04-10 17:55:57 -0300

    Squashed some bugs and added a parameter parser
    * Fixed error on issue #1, but not in a way I like. It seems I could have
    done in a better way, but actually couldn't.
    * Added "support" to parameters. Now there's only one parameter:
    -o <filename>: Sets the file output to <filename>. Default: "a.txt"

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# noter
-Simple ascii file formatter
+# Noter
+Simple ascii file formatter.
+
+I had the idea of developing this program from my bad habit of taking class notes in plain text; this helps a lot to format and organize the files.
+
+## How to use
+noter [options] <filename>
+
+## Compiling
+Insert compile instructions here
+
--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,38 @@ char ch;
 void ps_main(std::ifstream& in, std::ofstream& out);
 void ps_func(std::ifstream& in, std::ofstream& out);
 
-void call(std::string func, std::ifstream& in, std::ofstream& out);
+void call(const std::string& func, std::ifstream& in, std::ofstream& out);
 
 int main(int argc, char *argv[])
 {
-	if(argc != 2) return 0;
+	std::string output_filename = "a.txt";
+	std::string input_filename;
 
-	std::ifstream in (argv[1]);
+	for(int i = 1; i != argc; ++i)
+	{
+		if(argv[i][0] == '-')
+		{
+			if(argv[i][1] == 'o')
+			{
+				++i;
+				output_filename = argv[i];
+
+			}
+		} else
+			input_filename = argv[i];
+	}
+
+	if(input_filename.empty())
+	{
+		std::cout << "Could not find a suitable filename to convert" << std::endl;
+
+		return 0;
+	}
+
+	std::ifstream in (input_filename);
 	if(in.is_open())
 	{
-		std::ofstream out ("a.out");
+		std::ofstream out (output_filename);
 
 		ps_main(in, out);
 
@@ -44,13 +66,22 @@ void ps_main(std::ifstream& in, std::ofstream& out)
 
 void ps_func(std::ifstream& in, std::ofstream& out)
 {
+	char ch;
+	in >> std::noskipws >> ch;
+	if(ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n' || ch == '\0')
+	{
+		out << '#' << ch;
+		return;
+	} else
+		in.putback(ch);
+
 	std::string func_name;
 	in >> std::noskipws >> func_name;
 
 	call(func_name, in, out);
 }
 
-void call(std::string func_name, std::ifstream& in, std::ofstream& out)
+void call(const std::string& func_name, std::ifstream& in, std::ofstream& out)
 {
 	if(func_name == "insert")
 	{
@@ -64,9 +95,6 @@ void call(std::string func_name, std::ifstream& in, std::ofstream& out)
 			other_in.close();
 		}
 	} else
-	{
-		std::cout << "No function defined with name " << func_name << " defined. Ignoring control character..." << std::endl;
 		out << '#' << func_name;
-	}
 }
 
